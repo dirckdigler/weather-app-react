2d9c7096acb3cffc1a9a891327b73d3142066feb
Edwin Vega <edwin.vega.moreno@gmail.com>
2019-03-10 16:38:38 -0500

    class 61

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import LocationsList from './components/LocationsList';
 
+const cities = [
+  'Buenos Aires,ar',
+  'Washington,us',
+  'Bogota,col',
+  'Ciudad de mexico,mx',
+  'Madrid,mx',
+  'Lima,pe',
+];
+
 class App extends Component {
   render() {
     return (
       <div className = 'App'>
-        <LocationsList></LocationsList>
+        <LocationsList cities={cities}></LocationsList>
       </div>
     );
   }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import WeatherLocation from './WeatherLocations';
+import PropTypes from 'prop-types';
 
-const LocationsList = () => (
+const LocationsList = ({ cities }) =>{
+  console.log(cities);
+  return (
     <div>
       <WeatherLocation city='Buenos aires,ar' />
       <WeatherLocation city='Bogota,col' />
       <WeatherLocation city='Madrid,es' />
     </div>
-);
+  );
+};
+
+LocationsList.propTypes = {
+  cities: PropTypes.array.isRequired,
+}
 
 export default LocationsList;
